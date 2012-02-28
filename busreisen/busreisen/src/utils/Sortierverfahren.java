b54c7e8e40f28c6f738c96649a0bbfa840e8855a
t.trinklein91@gmail.com <t.trinklein91@gmail.com@9d32a441-78c3-e3de-f669-b6bbb03f9d25>
2012-02-28 09:53:18 +0000

    Created: Package "Utils"; Classes: "DateiIO","KonsolenIO", "Sortierverfahren", "Suchverfahren"

---
+++ busreisen/busreisen/src/utils/Sortierverfahren.java
@@ -0,0 +1,90 @@
+package utils;
+/**
+ * Diese Klasse ist für die Sortierung der Daten zuständig
+ * 
+ * @author Thomas
+ * @version 28.02.2012
+ * 
+ */
+public class Sortierverfahren {
+	/**
+	 * Disese Methode Sortiert die Daten in aufsteigender Reihenfolge.
+	 * Es wird das Verfahren "SelectionSort" genutzt.
+	 * @param daten
+	 * 				Array von Strings
+	 */
+	public static void selectionSort(int daten[]){
+		int stelle=0;
+		for(int i = 0; i < daten.length-1; i++){
+			int min = daten[i];
+
+			for (int j=i+1; j<daten.length; j++) {
+				if(min>daten[j]){
+					stelle=j;
+					min=daten[j];
+					}
+			}
+			if(stelle>i){
+				int hilfsVar=daten[stelle];
+				daten[stelle] = daten[i];
+				daten[i] = hilfsVar;
+				}
+		        
+		    }
+		
+	}
+	/**
+	 * Disese Methode Sortiert die Daten in aufsteigender Reihenfolge.
+	 * Es wird das Verfahren "BubbleSort" genutzt.
+	 * 
+	 * @param daten
+	 * 				Array von Strings
+	 */
+	public static void bubbleSort(int daten[]){
+		int j;
+		for (j = 0; j < daten.length-1; j++){
+			for (int i = 0; i < daten.length-1; i++) {
+					if(daten[i]>daten[i+1]){
+						int hilfsVar=daten[i];
+						daten[i]=daten[i+1];
+						daten[i+1]=hilfsVar;
+					}
+				}
+			}
+	}
+	/**
+	 * Disese Methode Sortiert die Daten in aufsteigender Reihenfolge.
+	 * Es wird das Verfahren "InsertSort" genutzt.
+	 * 
+	 * @param daten
+	 * 				Array von Strings
+	 */
+	public static void insertionSort(int daten[]){
+		/* void insertionSort(double datan[])
+		 * 		Deklaration von i,j, hilfsVar
+		 * 		wiederhole für i mit Anfangswert 1, Endwert LängeArray *(=10) und Schtittweite 1
+		 * 			j=i
+		 * 			hilfsVar=daten[i]
+		 * 			solange (j>0) oder (daten[j-1]>hilfsVar)
+		 * 					daten[j] = daten[j-1]
+		 * 					j--                                     
+		 * 			daten[j] = hilfsVar
+		 */
+		int hilfsVar;
+		int i,j;
+		for (i = 1; i < daten.length; i++) {
+			j=i;	
+			hilfsVar=daten[i];
+			
+			while((j>0) && (daten[j-1]>hilfsVar)){
+			
+					daten[j]=daten[j-1];
+					j--;
+				
+				}
+				
+				daten[j]=hilfsVar;
+			
+			}
+	}
+}
