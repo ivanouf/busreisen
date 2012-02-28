6dda73712ec42952a7eb009f720330c6b4a66d65
t.trinklein91@gmail.com <t.trinklein91@gmail.com@9d32a441-78c3-e3de-f669-b6bbb03f9d25>
2012-02-28 09:32:36 +0000

    Implementation of the class "Bus".

--- trunk/busreisen/busreisen/src/model/Bus.java
+++ trunk/busreisen/busreisen/src/model/Bus.java
@@ -1,5 +1,104 @@
 package model;
-
+/**
+ * Diese Klasse beschreibt einen Bus des Reisebüros.
+ * @author Thomas
+ * @version 28.02.2012
+ * 
+ */
 public class Bus {
 
+	private int anzahlPlaetze;
+	private int anzahlFreiePlaetze;
+	private int woche;
+	private String reiseZiel;
+	
+	/**
+	 * Dieser Konstruktor initialisiert die Attribute mit Default-Werten
+	 */
+	public Bus(){
+		this.anzahlPlaetze = 0;
+		this.anzahlFreiePlaetze = 0;
+		this.woche = 0;
+		this.reiseZiel = "NoWhere";
+	}
+	/**
+	 * Diese Methode setzt die übergebenen Werte.
+	 * @param anzahlPlaetze 
+	 * 					Anzahl der Plätze als Integer
+	 * @param woche 
+	 * 					Woche als Integer
+	 * @param reiseZiel 
+	 * 					Reiseziel als String
+	 */
+	public Bus(int anzahlPlaetze, int woche, String reiseZiel){
+		this.anzahlPlaetze = anzahlPlaetze;
+		this.woche = woche;
+		this.reiseZiel = reiseZiel;
+		
+	}
+	/**
+	 * Diese Methode gibt die Anzahl der Plätze als Integer zurück
+	 * @return anzahlPlaetze
+	 * 					Anzahl der Plätze als Integer
+	 */
+	public int getAnzahlPlaetze() {
+		return anzahlPlaetze;
+	}
+	/**
+	 * Diese Methode setzt die Anzahl der Plätze
+	 * @param anzahlPlaetze 
+	 * 					Anzahl der Plätze als Integer
+	 */
+	public void setAnzahlPlaetze(int anzahlPlaetze) {
+		this.anzahlPlaetze = anzahlPlaetze;
+	}
+	/**
+	 * Diese Methode gibt die Anzahl der freien Plätze als Integer zurück
+	 * @return anzahlFreiePlaetze
+	 * 					Anzahl freier Plätze als Integer
+	 */
+	public int getAnzahlFreiePlaetze() {
+		return anzahlFreiePlaetze;
+	}
+	/**
+	 * Diese Methode setzt die Anzahl der freien Plätze
+	 * @param anzahlFreiePlaetze 
+	 * 					Anzahl freier Plätze als Integer
+	 */
+	public void setAnzahlFreiePlaetze(int anzahlFreiePlaetze) {
+		this.anzahlFreiePlaetze = anzahlFreiePlaetze;
+	}
+	/**
+	 * Diese Methode gibt die Woche als Integer zurück
+	 * @return woche
+	 * 					Woche als Integer
+	 */
+	public int getWoche() {
+		return woche;
+	}
+	/**
+	 * Diese Methode setzt die Woche
+	 * @param woche
+	 * 					Woche als Integer
+	 */
+	public void setWoche(int woche) {
+		this.woche = woche;
+	}
+	/**
+	 * Diese Methode gibt das Reiseziel als String zurück
+	 * @return reiseZiel
+	 * 					Reiseziel als String
+	 */
+	public String getReiseZiel() {
+		return reiseZiel;
+	}
+	/**
+	 * Diese Methode setzt das Reiseziel
+	 * @param reiseZiel
+	 * 					Reiseziel als String
+	 */
+	public void setReiseZiel(String reiseZiel) {
+		this.reiseZiel = reiseZiel;
+	}
+	
 }
