48a37ea56e2f9365eb1a85ba3157615b13ab8f09
philipp.willms <philipp.willms@9d32a441-78c3-e3de-f669-b6bbb03f9d25>
2012-03-01 07:57:55 +0000

    Saving customers in .csv-File enabled.

--- busreisen/busreisen/src/utils/DateiIO.java
+++ busreisen/busreisen/src/utils/DateiIO.java
@@ -1,14 +1,43 @@
-/**
- * 
- */
 package utils;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import model.Kunde;
+
 /**
- * Diese Klasse lädt und speichert die Daten 
+ * Diese Klasse lädt und speichert die Daten
+ * 
  * @author Thomas
- * @version 28.02.2012
+ * @version 01.03.2012
  * 
  */
 public class DateiIO {
 
+	private static String[] headers = { "Buchungsnummer", "Name", "Vorname",
+			"Adresse", "Telefonnummer", "Ziel", "Anzahl der Plätze" };
+
+	public static void saveKundenToCSV(String filename, Kunde[] kunden)
+			throws IOException {
+		File csv_file = new File(filename);
+		BufferedWriter bw = new BufferedWriter(new FileWriter(csv_file));
+		for (int i = 0; i < headers.length; i++) {
+			bw.write(headers[i] + ";");
+		}
+		bw.write("\n");
+		for (int i = 0; i < kunden.length; i++) {
+			Kunde kunde = kunden[i];
+			bw.write(kunde.getBuchungsnr() + ";");
+			bw.write(kunde.getName() + ";");
+			bw.write(kunde.getVorname() + ";");
+			bw.write(kunde.getAdresse() + ";");
+			bw.write(kunde.getTelefonnr() + ";");
+			bw.write(kunde.getReiseZiel() + ";");
+			bw.write(kunde.getBuchungsnr() + ";");
+			bw.write("\n");
+		}
+
+	}
 }
