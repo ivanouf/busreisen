f6f214595572f0287135c53dee3198023c042094
philipp.willms <philipp.willms@9d32a441-78c3-e3de-f669-b6bbb03f9d25>
2012-03-02 09:28:11 +0000

    Created class "Buchung".

---
+++ trunk/busreisen/busreisen/src/model/Buchung.java
@@ -0,0 +1,177 @@
+package model;
+
+/**
+ * Diese Klasse modelliert eine Buchung, die von einem MItarbeiter des
+ * Reisebüros in das Programm eingegeben wird.
+ * 
+ * @author Philipp
+ * @version 02.03.2012
+ */
+public class Buchung {
+
+	/**
+	 * Instanz von {@link Kunde}, auf die sich die Buchung bezieht.
+	 */
+	private Kunde kunde;
+
+	/**
+	 * Buchungsnummer für diesen Kunden
+	 */
+	private int buchungsnr;
+
+	/**
+	 * Falls die Buchung storniert wird, existiert eine Stornonummer. Ansonsten
+	 * hat dieses Attribut den Wert 0.
+	 */
+	private int stornonr;
+
+	/**
+	 * Reiseziel des Kunden
+	 */
+	private String reiseZiel;
+
+	/**
+	 * Vom Kunden gewünschte Reisewoche
+	 */
+	private int woche;
+
+	/**
+	 * Vom Kunden gebuchte Anzahl der Plätze
+	 */
+	private int plaetze;
+
+	/**
+	 * Im Konstruktor wird die Buchung über folgende Parameter definiert.
+	 * 
+	 * @param buchungsnr
+	 *            vom System zugewiesene Buchungsnummer
+	 * @param reiseZiel
+	 *            gewünschtes Reiseziel des Kunden
+	 * @param woche
+	 *            gebuchte Reisewoche
+	 * @param kunde
+	 *            Kunde, der die Reise gebucht hat
+	 */
+	public Buchung(int buchungsnr, String reiseZiel, int woche, Kunde kunde) {
+		this.buchungsnr = buchungsnr;
+		this.reiseZiel = reiseZiel;
+		this.woche = woche;
+		this.kunde = kunde;
+		this.stornonr = 0;
+	}
+
+	/**
+	 * Diese Methode git den Kunden zurück, zu dem die Buchnung gehört hat.
+	 * 
+	 * @return Instanz vom Typ {@link Kunde}
+	 */
+	public Kunde getKunde() {
+		return kunde;
+	}
+
+	/**
+	 * Diese Methode definiert den Kunden, zu dem die Buchnung gehört.
+	 * 
+	 * @param kunde
+	 *            Instanz vom Typ {@link Kunde}
+	 */
+	public void setKunde(Kunde kunde) {
+		this.kunde = kunde;
+	}
+
+	/**
+	 * Diese Methode gibt die Buchungsnummer zurück.
+	 * 
+	 * @return Buchungsnummer als ganze Zahl
+	 */
+	public int getBuchungsnr() {
+		return buchungsnr;
+	}
+
+	/**
+	 * Diese Methode definiert die Buchungsnummer.
+	 * 
+	 * @param buchungsnr
+	 *            Buchungsnummer als ganze Zahl
+	 */
+	public void setBuchungsnr(int buchungsnr) {
+		this.buchungsnr = buchungsnr;
+	}
+
+	/**
+	 * Diese Methode gibt die Stornonummer zurück.
+	 * 
+	 * @return Stornonummer als ganze Zahl
+	 */
+	public int getStornonr() {
+		return stornonr;
+	}
+
+	/**
+	 * Diese Methode definiert die Stornonummer.
+	 * 
+	 * @param stornonr
+	 *            Stornonummer als ganze Zahl
+	 */
+	public void setStornonr(int stornonr) {
+		this.stornonr = stornonr;
+	}
+
+	/**
+	 * Diese Methode gibt das Reiseziel zurück.
+	 * 
+	 * @return Reiseziel als String
+	 */
+	public String getReiseZiel() {
+		return reiseZiel;
+	}
+
+	/**
+	 * Diese Methode definiert das Reiseziel.
+	 * 
+	 * @param reiseZiel
+	 *            Reiseziel als String
+	 */
+	public void setReiseZiel(String reiseZiel) {
+		this.reiseZiel = reiseZiel;
+	}
+
+	/**
+	 * Diese Methode gibt die Woche zurück.
+	 * 
+	 * @return Woche als ganze Zahl
+	 */
+	public int getWoche() {
+		return woche;
+	}
+
+	/**
+	 * Diese Methode definiert die Woche der Buchung.
+	 * 
+	 * @param woche
+	 *            Woche als ganze Zahl
+	 */
+	public void setWoche(int woche) {
+		this.woche = woche;
+	}
+
+	/**
+	 * Diese Methode gibt die Anzahl der gebuchten Plätze zurück.
+	 * 
+	 * @return Anzahl der Plätze als ganze Zahl
+	 */
+	public int getPlaetze() {
+		return plaetze;
+	}
+
+	/**
+	 * Diese Methode definiert die Anzahl der gebuchten Plätze.
+	 * 
+	 * @param plaetze
+	 *            Anzahl der Plätze als ganze Zahl
+	 */
+	public void setPlaetze(int plaetze) {
+		this.plaetze = plaetze;
+	}
+
+}
